856303ebd8f97355898bd9bb3baaa50b02fa1959
dewbeeny <dewbeeny@gmail.com>
2025-05-07 09:47:53 +0900

    feat: voice 레이아웃 디테일 최종 완성

--- client/src/sidepanel/pages/Voice.styles.tsx
+++ client/src/sidepanel/pages/Voice.styles.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: relative;
   height: 100vh;
-  padding: 3rem 1rem 2.5rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -11,26 +10,29 @@ export const Container = styled.div`
 `;
 
 export const TextBox = styled.div`
-  margin-top: 4rem;
+  margin-top: 18rem;
   text-align: center;
 `;
 
 export const Text = styled.p`
-  font-size: 1.25rem;
-  font-weight: 500;
-  line-height: 1.8;
+  font-size: 1.25rem; /* 20px */
+  font-family: var(--font-main);
+  line-height: 3.125rem; /* 50px */
 `;
 
 export const Spoken = styled.span`
   color: #000000;
+  font-weight: 400;
 `;
 
 export const Suggested = styled.span`
-  color: #0970E7;
+  color: rgba(9, 112, 231, 0.61);
+  font-weight: 200;
 `;
 
+
 export const WaveWrapper = styled.div`
-  position: relative;
+  position: center;
   width: 100%;
   height: 120px;
   margin-top: auto;
